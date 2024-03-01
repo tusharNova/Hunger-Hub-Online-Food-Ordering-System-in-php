56bc9fa416c8eb8e4f1a625436a933531e6fa2a8
tusharNova <tusharmankar02@gmail.com>
2024-03-02 01:16:07 +0530

    dashboard cards added

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -41,11 +41,17 @@
   .sidebar-link[aria-expanded="true"] .right-icon {
     transform: rotate(180deg);
   }
+
+  @media (min-width:992px) {
+    main {
+      margin-left: 270px;
+    }
+  }
 </style>
 
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container-fluid">
       <!-- offcanvas   button-->
       <button class="navbar-toggler me-4" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
@@ -62,8 +68,6 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
         <form class="d-flex ms-auto">
-          <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button> -->
         </form>
         <ul class="navbar-nav  mb-2 mb-lg-0 ml-2">
           <li class="nav-item dropdown">
@@ -84,16 +88,12 @@
       </div>
     </div>
   </nav>
-
   <!-- left Dashboard -->
+
   <!-- offcanvas  -->
   <div class="offcanvas offcanvas-start sidebar-nav bg-dark text-white" tabindex="-1" id="offcanvasExample"
     aria-labelledby="offcanvasExampleLabel">
-    <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-      <button type="button" class="btn-close text-reset bg-white" data-bs-dismiss="offcanvas"
-        aria-label="Close"></button>
-    </div>
+
     <div class="offcanvas-body">
       <nav class="navbar-dark">
         <ul class="navbar-nav">
@@ -178,12 +178,100 @@
           </li>
         </ul>
       </nav>
-
-
-
     </div>
   </div>
   <!-- offcanvas  -->
+
+
+  <main class="mt-5 p-3">
+    <div style="padding-top: 10px;" class="text-muted fw-bold mb-3">
+      <marquee onMouseOver="this.stop()" onMouseOut="this.start()"> This website is created by tushar m.</marquee>
+    </div>
+    <div class="container-fluid shadow p-5 border border-2 rounded-2">
+      <div class="row">
+        <div class="col-md-12 fw-bold fs-3 bg-primary text-white rounded-2">Dashboard</div>
+      </div>
+      <div class="row mt-2">
+        <div class="col-md-3 p-2">
+          <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-body">Content</div>
+            <div class="card-footer">Footer</div>
+          </div>
+        </div>
+        <div class="col-md-3 p-2">
+          <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-body">Content</div>
+            <div class="card-footer">Footer</div>
+          </div>
+        </div>
+        <div class="col-md-3 p-2">
+          <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-body">Content</div>
+            <div class="card-footer">Footer</div>
+          </div>
+        </div>
+        <div class="col-md-3 p-2">
+          <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-body">Content</div>
+            <div class="card-footer">Footer</div>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-2">
+        <div class="col-md-4 p-2">
+          <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-body">Content</div>
+            <div class="card-footer">Footer</div>
+          </div>
+        </div>
+        <div class="col-md-4 p-2">
+          <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-body">Content</div>
+            <div class="card-footer">Footer</div>
+          </div>
+        </div>
+        <div class="col-md-4 p-2">
+          <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-body">Content</div>
+            <div class="card-footer">Footer</div>
+          </div>
+        </div>
+
+      </div>
+      <div class="row mt-2">
+        <div class="col-md-4 p-2">
+          <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-body">Content</div>
+            <div class="card-footer">Footer</div>
+          </div>
+        </div>
+        <div class="col-md-4 p-2">
+          <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-body">Content</div>
+            <div class="card-footer">Footer</div>
+          </div>
+        </div>
+        <div class="col-md-4 p-2">
+          
+        </div>
+       
+      </div>
+    </div>
+
+  </main>
+  <!-- Dashboard -->
+
+  <!-- Dashboard -->
+
 </body>
 
 </html>
