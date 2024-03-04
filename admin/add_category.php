93eca1c4ac6a7ada9d1d3690374ae000fe93d094
tusharNova <tusharmankar02@gmail.com>
2024-03-05 01:21:46 +0530

    add table to data

--- admin/add_category.php
+++ admin/add_category.php
@@ -2,71 +2,109 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>All Category</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>All Category</title>
+  <!-- <script src="js/sweetalert.min.js"></script> -->
+  <script src="../js/sweetalert.min.js"></script>
+  <script src="../js/sweetalert2@11.js"></script>
+
+  <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.22.3/dist/bootstrap-table.min.css">
+  <script src="https://unpkg.com/bootstrap-table@1.22.3/dist/bootstrap-table.min.js"></script>
 </head>
 <style>
-    @media (min-width:992px) {
-        .leftside {
-            margin-left: 270px;
-        }
+  @media (min-width:992px) {
+    .leftside {
+      margin-left: 270px;
     }
+  }
 </style>
 
 <body>
-    <?php include_once('navbar.php'); ?>
+  <?php include_once('navbar.php'); ?>
 
-    <div class="leftside p-5">
-        <div class="container  border  border-3 p-2 ">
-            <div class="bg-primary h4 text-white rounded-2 text-center p-3">Add Restaurant Category</div>
-            <hr>
-            <div class="mt-4 h5">Category</div>
-            <input type="text" class="col-md-12 mt-4  form-control form-control-lg">
-            <div class="mt-4">
-                <button type="button" class="btn btn-primary">Save</button>
-                <button type="button" class="btn btn-secondary">Cancel</button>
-            </div>
-        </div>
-
-        <div class="container  border  border-3 p-2 mt-5">
-            Listed Categories
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores optio omnis iusto ex? Ex vitae deleniti, est, unde harum asperiores optio numquam quibusdam consequatur doloremque, id modi neque. Magni, molestias.
-            </div>
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
-                </tbody>
-              </table>
-        </div>
-
-       
+  <div class="leftside p-5">
+    <div class="container  border  border-3 p-2 ">
+      <div class="bg-primary h4 text-white rounded-2 text-center p-3">Add Restaurant Category</div>
+      <hr>
+      <div class="mt-4 h5">Category</div>
+      <input type="text" class="col-md-12 mt-4 form-control form-control-lg" id="txtcategory">
+      <div class="mt-4">
+        <button type="button" class="btn btn-primary" onclick="addcategory()">Save</button>
+        <button type="button" class="btn btn-secondary">Cancel</button>
+      </div>
     </div>
+
+    <div class="container  border  border-3 p-2 mt-5">
+      Listed Categories
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores optio omnis iusto ex? Ex vitae deleniti, est,
+        unde harum asperiores optio numquam quibusdam consequatur doloremque, id modi neque. Magni, molestias.
+      </div>
+      <div class="table-responsive m-t-40">
+        <table id="myTable" class="table table-bordered table-hover table-striped">
+          <thead class="thead-dark">
+            <tr>
+              <th>ID</th>
+              <th>Category Name</th>
+              <th>Date</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $sql = "SELECT * FROM `res_category`";
+            $query = mysqli_query($conn, $sql);
+            if (!mysqli_num_rows($query) > 0) {
+              echo '<td colspan="7"><center>No Categories-Data!</center></td>';
+            } else {
+              while ($rows = mysqli_fetch_array($query)) {
+                echo ' <tr><td>' . $rows['c_id'] . '</td>
+                        <td>' . $rows['c_name'] . '</td>
+                        <td>' . $rows['date'] . '</td>
+                        
+                           <td><a class="btn btn-danger  btn-xs m-b-10" id="btndelete"><i class="fa fa-trash-o" style="font-size:16px" ></i></a> 
+                           <a  class="btn btn-info  btn-sm m-b-10 m-l-5" id="btnedit"><i class="fa fa-edit"></i></a>
+                          </td></tr>';
+              }
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </body>
+<script>
+  function addcategory() {
+    if ($("#txtcategory").val() == "") {
+      alert("invalid Enter")
+    } else {
+      $.post("code.php", {
+        cmd: "addcategory",
+        item: $("#txtcategory").val(),
+      }, function(data, status) {
+        if (data == "1") {
+          Swal.fire({
+            text: "Category Added",
+            icon: "success"
+          });
+        } else {
+          Swal.fire({
+            text: "Failed to add",
+            icon: "error"
+          });
+        }
+      });
+      location.reload();
+
+    }
+  }
+
+  $("#btndelete").click(function () { 
+    alert("delete data");
+   })
+
+</script>
 
 </html>
--- admin/code.php
+++ admin/code.php
@@ -7,29 +7,57 @@ session_start();
 $cmd = $_POST["cmd"];
 
 # for admin login
-if($cmd === "adminlogin"){
+if ($cmd === "adminlogin") {
     $username = $_POST['user'];
     $password = $_POST['pswd'];
-   
-    
+
+
     $sql = "SELECT * FROM `admin` where username='$username' AND password='$password'";
     $result = mysqli_query($conn, $sql);
     $cnt = mysqli_num_rows($result);
     $row = mysqli_fetch_array($result);
-    if (is_array($row)){
+    if (is_array($row)) {
         $_SESSION["adminname"] = $row['username'];
         echo "$cnt";
-    }
-    else{
+    } else {
         echo "Invalid Username or Password";
     }
 }
 # for change password
-if($cmd ==="changeadminpass"){
-    echo"ok";
-} 
+if ($cmd === "changeadminpass") {
+    echo "ok";
+}
 
 # for add Category 
-if($cmd === "addcategory"){
-    echo "sdfsdf";
+if ($cmd === "addcategory") {
+    $item  = $_POST['item'];
+    // echo "$item";
+
+    $sql = "INSERT INTO `res_category`(`c_id`, `c_name`, `date`) VALUES (null,'$item',CURRENT_TIMESTAMP)";
+    $run = mysqli_query($conn, $sql);
+    if ($run) {
+        echo  "1";
+    } else {
+        echo "0";
+    }
+}
+# for list of Category
+if ($cmd === "listCategory") {
+
+    $sql = "SELECT * FROM `res_category`";
+    $result = mysqli_query($conn, $sql);
+    $arr = array();
+    while ($row = mysqli_fetch_array($result)) {
+
+        $record = array(
+            'id' => $row[0],
+            'c_name' => $row[1],
+            'time' => $row[2],
+            'List' => "<button class='btn btn-info' id='btnlist' onclick='' value='$row[0]' >List</button>",
+            'Update' => "<button class='btn btn-info' id='btnUpdate' value='$row[0]' >Update</button>"
+
+        );
+        array_push($arr, $record);
+    }
+    echo json_encode($arr);
 }
