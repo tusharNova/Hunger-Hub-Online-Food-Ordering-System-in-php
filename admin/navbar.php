d9d064a7e78b34ac0ee62fa14684006a6840ca16
Tushar Mankar <tusharmankar02@gmail.com>
2024-03-07 19:20:12 +0530

    add sweetalert

--- admin/navbar.php
+++ admin/navbar.php
@@ -16,7 +16,8 @@ if(empty( $_SESSION["adminname"]))
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- css -->
-
+  <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.22.3/dist/bootstrap-table.min.css">
+  <script src="https://unpkg.com/bootstrap-table@1.22.3/dist/bootstrap-table.min.js"></script>
 
   <link rel="stylesheet" href="../css/bootstrap.min.css">
   <link rel="stylesheet" href="../css/bootstrap-icons.css">
@@ -36,6 +37,9 @@ if(empty( $_SESSION["adminname"]))
     integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
+
+  <script src="../js/sweetalert.min.js"></script>
+  <script src="../js/sweetalert2@11.js"></script>
 </head>
 <style>
   .sidebar-link {
