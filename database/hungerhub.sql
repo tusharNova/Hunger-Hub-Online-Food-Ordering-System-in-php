06dd6bcd16b13698b61914cadf17705277d5ac7c
Tushar Mankar <tusharmankar02@gmail.com>
2024-03-07 19:18:48 +0530

    restaurant table add

--- database/hungerhub.sql
+++ database/hungerhub.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2024 at 08:52 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: Mar 07, 2024 at 02:47 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,6 +45,34 @@ INSERT INTO `admin` (`adm_id`, `username`, `password`, `date`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `restaurant`
+--
+
+CREATE TABLE `restaurant` (
+  `r_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `email` int(11) NOT NULL,
+  `phone` text NOT NULL,
+  `url` int(11) NOT NULL,
+  `o_hr` int(11) NOT NULL,
+  `c_hr` int(11) NOT NULL,
+  `o_days` text NOT NULL,
+  `address` text NOT NULL,
+  `image` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`r_id`, `c_id`, `title`, `email`, `phone`, `url`, `o_hr`, `c_hr`, `o_days`, `address`, `image`, `date`) VALUES
+(1, 0, 'Shree ', 0, '96325874120', 0, 10, 10, 'every day', 'pt-123123', 'sdfsdfs', '2024-03-07 13:11:43');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `res_category`
 --
 
@@ -59,9 +87,11 @@ CREATE TABLE `res_category` (
 --
 
 INSERT INTO `res_category` (`c_id`, `c_name`, `date`) VALUES
-(1, 'South indian', '2024-03-04 19:45:19'),
-(2, 'Maharashtrian', '2024-03-04 19:46:38'),
-(3, 'gujarati', '2024-03-04 19:46:52');
+(1, 'Maharashtrian', '2024-03-07 13:31:17'),
+(2, 'North indian', '2024-03-07 13:38:35'),
+(3, 'South indian', '2024-03-07 13:38:50'),
+(4, 'Gujarati', '2024-03-07 13:39:03'),
+(5, 'Punjabi', '2024-03-07 13:39:28');
 
 -- --------------------------------------------------------
 
@@ -100,6 +130,12 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`adm_id`);
 
 --
+-- Indexes for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`r_id`);
+
+--
 -- Indexes for table `res_category`
 --
 ALTER TABLE `res_category`
@@ -122,10 +158,16 @@ ALTER TABLE `admin`
   MODIFY `adm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `res_category`
 --
 ALTER TABLE `res_category`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
