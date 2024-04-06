8edfc41a059f6ab4a58cc368ef4ec4777916a684
Tushar Mankar <tusharmankar02@gmail.com>
2024-04-06 19:29:23 +0530

    add names

--- admin/footer.php
+++ admin/footer.php
@@ -1,1 +1,1 @@
-<footer class="footer mt-5" style="text-align: center;">Hunger Hub TB © 2024 - Online Food Ordering System Developer - <a>Tushar</a> </footer>
+<footer class="footer mt-5" style="text-align: center;">Hunger Hub TB © 2024 - Online Food Ordering System Developer - <a>Tushar & Bharti</a> </footer>
