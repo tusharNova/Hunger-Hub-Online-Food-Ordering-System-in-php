ff9f87264da9da5e5b6136291ff706a594384584
Tushar Mankar <tusharmankar02@gmail.com>
2024-04-07 15:58:45 +0530

    create cards

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,9 +2,8 @@
 include("../connection/connect.php");
 error_reporting(0);
 session_start();
-if(empty( $_SESSION["adminname"]))
-{
-	header('location:index.php');
+if (empty($_SESSION["adminname"])) {
+  header('location:index.php');
 }
 ?>
 
@@ -25,22 +24,89 @@ if(empty( $_SESSION["adminname"]))
 </style>
 
 <body>
-  <?php  include_once("navbar.php"); ?>
+  <?php include_once("navbar.php"); ?>
   <!-- Main Content -->
   <div class="leftside p-5">
     <div class="container  border  border-3 p-2 ">
       <div class="bg-primary h4 text-white rounded-2 text-center p-3">Dashboard</div>
       <hr>
       <div class="row">
-        <div class=""></div>
-        <div class="card text-white bg-info  mb-3" style="max-width: 18rem;">
-          <div class="card-header">Header</div>
-          <div class="card-body">
-            <h5 class="card-title">Warning card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-          </div>
 
+        <div class="col-md-3">
+          <div class="card p-30">
+            <div class="media">
+              <div class="media-left meida media-middle">
+                <span><i class="fa fa-home f-s-40 "></i></span>
+              </div>
+              <div class="media-body media-text-right">
+                <h2>
+                  <?php $sql = "select * from restaurant";
+                  $result = mysqli_query($conn, $sql);
+                  $rws = mysqli_num_rows($result);
+
+                  echo $rws; ?>
+                </h2>
+                <p class="m-b-0">Restaurants</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="card p-30">
+            <div class="media">
+              <div class="media-left meida media-middle">
+                <span><i class="fa fa-cutlery f-s-40" aria-hidden="true"></i></span>
+              </div>
+              <div class="media-body media-text-right">
+                <h2>
+                  <?php $sql = "select * from dishes";
+                  $result = mysqli_query($conn, $sql);
+                  $rws = mysqli_num_rows($result);
+
+                  echo $rws; ?>
+                </h2>
+                <p class="m-b-0">Dishes</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card p-30">
+            <div class="media">
+              <div class="media-left meida media-middle">
+                <span><i class="fa fa-users f-s-40"></i></span>
+              </div>
+              <div class="media-body media-text-right">
+                <h2><?php $sql = "select * from user";
+                    $result = mysqli_query($conn, $sql);
+                    $rws = mysqli_num_rows($result);
+
+                    echo $rws; ?></h2>
+                <p class="m-b-0">Users</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card p-30">
+            <div class="media">
+              <div class="media-left meida media-middle">
+                <span><i class="fa fa-shopping-cart f-s-40" aria-hidden="true"></i></span>
+              </div>
+              <div class="media-body media-text-right">
+                <h2>
+                  <?php
+                  # $sql = "select * from users_orders";
+                   # $result = mysqli_query($conn, $sql);
+                    #$rws = mysqli_num_rows($result);
+                    #echo $rws; 
+                    #?>
+                     3</h2>
+                <p class="m-b-0">Total Orders</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
