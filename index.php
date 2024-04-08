906bb72b7c53904d7592e23da0ca8b633c4423a0
Tushar Mankar <tusharmankar02@gmail.com>
2024-04-08 13:27:58 +0530

    navbar

--- index.php
+++ index.php
@@ -1,15 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/bootstrap-icons.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-    
-    <div class="container">sdfdsd</div>
+    <!-- // Navbar -->
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <div class="container-fluid ">
+            <a class="navbar-brand" href="#">
+                <img src="images/logo.png" alt="Avatar Logo" style="width:40px;" class="rounded-pill">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Restaurants</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="row">
+        <div class="col-dm-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quod aut incidunt accusantium et, porro ullam nostrum possimus asperiores quo fugit earum corporis eligendi amet dolores? Inventore optio ut laborum! Obcaecati ex cupiditate quidem architecto minima explicabo nemo! Temporibus autem, quae distinctio alias incidunt aperiam molestias quaerat dolores modi atque deleniti laudantium error tempora, impedit vitae consectetur voluptatum, laboriosam sequi est. Accusantium optio fuga quo nam itaque ipsum dolorum provident. Repellendus saepe quam ducimus corrupti quia repellat ipsa debitis architecto aut quidem facere, quisquam natus minima quibusdam est itaque ratione tempora incidunt! Magnam minima commodi temporibus vel illo laudantium officia.</div>
+        <div class="col-dm-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium accusamus fuga fugiat praesentium est quaerat illum nobis voluptate iure modi, debitis porro unde quod saepe, temporibus dolores veniam reiciendis soluta dolorum amet! At eaque aliquid nam deserunt, tempora molestias blanditiis! Quos odit nostrum hic? Neque asperiores facere facilis dolorem tempora dignissimos ex omnis possimus dicta incidunt placeat quasi atque odit quia veritatis fugiat excepturi nam maiores deserunt suscipit qui ea illum, quidem mollitia? Omnis numquam dolor architecto, praesentium libero asperiores ab, provident alias aliquam soluta ea pariatur odio. Odio aspernatur aliquam nulla soluta totam quos blanditiis excepturi nemo! Veritatis, nisi!</div>
+    </div>
 </body>
+
 </html>
