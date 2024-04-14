7e28024566e221b9df2da965123bbbcf52ac3c79
Tushar Mankar <tusharmankar02@gmail.com>
2024-04-14 21:40:06 +0530

    add nav link

--- navbar.php
+++ navbar.php
@@ -17,7 +17,7 @@
                     </form>
                     <ul class="navbar-nav  mb-2 mb-lg-0 ml-2">
                         <li class="nav-item">
-                            <a class="nav-link active " aria-current="page" href="#">Home</a>
+                            <a class="nav-link active " aria-current="page" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link active" href="#">Restaurants</a>
